e28f4f75d1d0ad8ee82fcd3997f0fed5d969e01a
mrashutoshkrsingh <mrashutoshrsingh@gmail.com>
2018-09-05 11:35:46 +0530

    modified

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express=require("express"),
 app=express.Router();
 
 app.get("/",(req,res)=>{
-  res.render("index/welcome")
+  res.render("./index/welcome")
 })
 
 app.get("/dashboard",(req,res)=>{
